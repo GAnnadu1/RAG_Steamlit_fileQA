250f2a00a4b0e2449f6324a1c42cacae181c6b6a
GAnnadu1 <126003327+GAnnadu1@users.noreply.github.com>
2026-09-01 12:10:40 +0200

    Refactor save path assignment for readability

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ uploaded_file = st.file_uploader("Upload your PDF Document", type="pdf")
 
 if uploaded_file is not None:
     #define save path
-    save_path = os.path.join(working_dir, uploaded_file.name)
+    save_path = os.path.join(
+        working_dir, 
+        uploaded_file.name
+    )
     #Save the file
     with open(save_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
