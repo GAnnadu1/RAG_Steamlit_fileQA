5335e258a54424ec025c5c9e0fdf58964c530203
GAnnadu1 <126003327+GAnnadu1@users.noreply.github.com>
2026-09-01 11:51:24 +0200

    Refactor PDF upload and processing logic
    Refactor file upload handling and update response display.

--- app.py
+++ app.py
@@ -13,22 +13,20 @@ uploaded_file = st.file_uploader("Upload your PDF Document", type="pdf")
 
 if uploaded_file is not None:
     #define save path
-    file_path = os.path.join(working_dir, uploaded_file.name)
+    save_path = os.path.join(working_dir, uploaded_file.name)
     #Save the file
-    with open(file_path, "wb") as f:
+    with open(save_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
     
-    st.write(f"Processing {uploaded_file.name}...")
-    process_document_to_chroma_db(uploaded_file.name)
-    st.write("Document processing complete.")
+    process_document = process_document_to_chroma_db(uploaded_file.name)
+    st.info("Document processing Successfully.")
 
 # Question input
 user_question = st.text_input("Ask a question about the document:")
 
-if user_question:
-    if 'db' not in st.session_state:
-        st.error("Please upload and process a document first.")
-    else:
-        st.write("Generating response...")
-        response = answer_question(user_question)
-        st.write(response)
+if st.button("Answer"):
+    
+    answer = answer_question(user_question)
+
+    st.markdown("### GenAI Response")
+    st.markdown(answer)
